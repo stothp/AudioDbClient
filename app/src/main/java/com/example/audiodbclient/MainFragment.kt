8ca7c9c03e92a81224eb4b0be3ce9731199aa528
stothp <stothp@gmail.com>
2020-12-13 20:57:02 +0100

    Initial

---
+++ app/src/main/java/com/example/audiodbclient/MainFragment.kt
@@ -0,0 +1,83 @@
+package com.example.audiodbclient
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.audiodbclient.databinding.FragmentMainBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(
+            inflater,
+            R.layout.fragment_main, container, false
+        )
+
+        binding.button.setOnClickListener{ view ->
+            run {
+                Log.i ("Info", binding.artistName.text.toString())
+                if (binding.artistName.text.toString().trim().isEmpty()){
+//                    var toast: Toast = Toast.makeText(activity?.applicationContext, "Töltsd ki az előadót!", Toast.LENGTH_LONG)
+//                    toast.setGravity(Gravity.TOP or Gravity.CENTER, 0, 0)
+//                    toast.show()
+                } else {
+                    view.findNavController()
+                        .navigate(R.id.action_mainFragment_to_artistListFragment)
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
