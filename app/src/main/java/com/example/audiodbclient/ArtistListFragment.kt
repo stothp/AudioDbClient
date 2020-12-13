2c07bfd682434f397fcd1c91f7acff3ae5ef13ac
stothp <stothp@gmail.com>
2020-12-13 21:00:11 +0100

    ArtistListFragment fixed

--- app/src/main/java/com/example/audiodbclient/ArtistListFragment.kt
+++ app/src/main/java/com/example/audiodbclient/ArtistListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.audiodbclient.databinding.FragmentArtistListBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,10 @@ class ArtistListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        var binding = DataBindingUtil.inflate<FragmentArtistListBinding>(
+            inflater, R.layout.fragment_artist_list, container, false)
 
+        return binding.root
     }
 
     companion object {
